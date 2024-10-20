ef0c16d33cc9e8081eaf570fa16a3a97f9617e68
Nadiia Yuce <soyka9616@gmail.com>
2024-10-20 10:36:25 +0200

    task-3

---
+++ src/scripts/addOneProduct.js
@@ -0,0 +1,13 @@
+import { createFakeProduct } from '../utils/createFakeProduct.js';
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/products.js';
+
+export const addOneProduct = async () => {
+  const products = await fs.readFile(PATH_DB, 'utf-8');
+  const parsedProducts = JSON.parse(products);
+  const newProduct = createFakeProduct();
+  parsedProducts.push(newProduct);
+  fs.writeFile(PATH_DB, JSON.stringify(parsedProducts, null, 2));
+};
+
+addOneProduct();
