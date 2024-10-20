76cc573ab9261b5f0ce503db5d5117244c9f6ceb
Hanna Lakus <annalakus@ukr.net>
2024-10-20 10:03:57 +0100

    task 5

---
+++ src/scripts/countProducts.js
@@ -0,0 +1,9 @@
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/products.js';
+
+const countProducts = async () => {
+  const products = await fs.readFile(PATH_DB, 'utf-8');
+  const parsedProduct = JSON.parse(products);
+  return parsedProduct.length;
+};
+console.log(await countProducts());
