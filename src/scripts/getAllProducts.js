9a68104a47d74d8d1b561e92d247e9c1db9e7681
melodiam16 <121240405+melodiam16@users.noreply.github.com>
2024-10-20 10:49:34 +0200

    task-4

---
+++ src/scripts/getAllProducts.js
@@ -0,0 +1,9 @@
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/products.js';
+const getAllProducts = async () => {
+  const data = await fs.readFile(PATH_DB, 'utf-8');
+  const dataParse = JSON.parse(data);
+  return dataParse;
+};
+
+console.log(await getAllProducts());
