e45fbf46513984cf69154b8f9a670cff6674e7c0
Serhii Reshetniak <reshet@lozovaya.org.ua>
2024-11-17 20:29:15 +0200

    Finish task 3 (validation)

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { registerUsersController } from '../controllers/users.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { registerUserSchema } from '../validation/users.js';
 const router = new Router();
 
-router.post('/register', ctrlWrapper(registerUsersController));
+router.post(
+  '/register',
+  validateBody(registerUserSchema),
+  ctrlWrapper(registerUsersController),
+);
 
 export default router;
